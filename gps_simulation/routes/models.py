2a8c2faf7c79c380adfddcfeb045bb7d6a7f7bd0
flavianoelia <flavianoelia79@gmail.com>
2024-10-30 19:10:14 -0300

    Modificaciones en modelo city, url, views, admin, home, city_detail, base

--- gps_simulation/routes/models.py
+++ gps_simulation/routes/models.py
@@ -1,14 +1,34 @@
 from django.db import models
 
+
 class City(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    image = models.ImageField(
+        upload_to="../gps_simulation/media/city_images", null=True, blank=True
+    )
 
     def __str__(self):
         return self.name
 
+    """
+class TouristPlace(models.Model):# Nueva clase
+    city = models.ForeignKey(City, related_name='lugares_turisticos', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+"""
+
+    def __str__(self):
+        return self.name
+
+
 class Route(models.Model):
-    start_city = models.ForeignKey(City, related_name='route_start', on_delete=models.CASCADE)
-    end_city = models.ForeignKey(City, related_name='route_end', on_delete=models.CASCADE)
+    start_city = models.ForeignKey(
+        City, related_name="route_start", on_delete=models.CASCADE
+    )
+    end_city = models.ForeignKey(
+        City, related_name="route_end", on_delete=models.CASCADE
+    )
     distance = models.FloatField()
 
     def __str__(self):
--- gps_simulation/routes/urls.py
+++ gps_simulation/routes/urls.py
@@ -1,9 +1,18 @@
+
+from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings  #
+from django.conf.urls.static import static  # 
+#from .views import home, shortest_route, load_graph, city_detail
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('load/', views.load_graph, name='load_graph'),
     path('shortest-route/', views.shortest_route, name='shortest_route'),
     path('city/<int:id>/', views.city_detail, name='city_detail'),
 ]
+# Agregue esta línea para servir archivos media en modo desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
