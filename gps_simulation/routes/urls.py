7aa73e03545e60bfdfef11fa64461d39e5e266a5
flavianoelia <flavianoelia79@gmail.com>
2024-11-18 18:43:09 -0300

    Modificqué los dos archivos urls.py el del directorio raiz y el de routes porque en este último solo iban las rutas de la aplicación y yo allí habia incluido la configuración de los archivos static, ahora esta en el lugar correcto.

--- gps_simulation/routes/urls.py
+++ gps_simulation/routes/urls.py
@@ -4,10 +4,9 @@ from django.urls import path #
 from . import views
 from django.conf import settings  #
 from django.conf.urls.static import static  # 
-#from .views import home, shortest_route, load_graph, city_detail
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('load/', views.load_graph, name='load_graph'),
     path('shortest-route/', views.shortest_route, name='shortest_route'),
@@ -15,7 +14,3 @@ urlpatterns = [
     path('tourist-places/', views.tourist_places, name='tourist_places'),
     path('contact/', views.contact, name='contact'),
 ]
-# Agregue esta línea para servir archivos media en modo desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
